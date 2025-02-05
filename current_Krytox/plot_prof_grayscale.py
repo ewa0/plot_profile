fbab9f72b4c73d8255890aa5736fb845e027a440
Ewa <e.sroczyk@interia.pl>
2025-02-05 17:10:41 -0500

    Separate calculation of the phases fractions in the pore, the exit reservoir, and the inlet reservoir

--- current_Krytox/plot_prof_grayscale.py
+++ current_Krytox/plot_prof_grayscale.py
@@ -5,13 +5,13 @@ import numpy as np
 import pandas as pd
 
 i = 0
-dim05 = [916, 20, 39, 600]
-dim1 = [880, 20, 71, 600]
-dim2 = [868, 20, 95, 600]
-dim3 = [856, 20, 119, 600]
+dim05 = [916, 20, 59, 600]
+dim1 = [880, 20, 71, 775]
+dim2 = [868, 20, 95, 775]
+dim3 = [856, 20, 119, 775]
 
 # folder_name = "test"  # change here
-folder_name = "pore_diam_3um"  # change here
+folder_name = "pore_diam_05um"  # change here
 dimensions = dim05  # change here
 
 # folder_name = "pore_diam_05um"
@@ -41,7 +41,7 @@ for filename in file_list:
         cropped_image = img.crop((x, y, x + width, y + height))
         # cropped_image.show()
 
-        # plot profile
+        # plot profile all
         all_values = np.asarray(cropped_image.getdata())
         values = all_values[:, 0]
 
@@ -51,18 +51,51 @@ for filename in file_list:
         cumul_none = len(list((filter(lambda none: 248 <= none, values))))  # white
         # print(filename)
 
+        # cropping pore only
+        cropped_pore = cropped_image.crop((0, 358, width, 358 + 238))
+        # cropped_pore.show()
+
+        # plot profile pore
+        pore_values = np.asarray(cropped_pore.getdata())
+        values_pore = pore_values[:, 0]
+
+        # attributing grayscale value to a phase
+        pore_dodecane = len(list((filter(lambda dod: dod <= 40, values_pore))))  # black
+        pore_krytox = len(list((filter(lambda kryt: 41 <= kryt <= 247, values_pore))))  # gray
+        pore_none = len(list((filter(lambda none: 248 <= none, values_pore))))  # white
+
+        # cropping top reservoir
+        cropped_exit_reservoir = cropped_image.crop((0, 0, width, 357))
+        # cropped_exit_reservoir.show()
+
+        # plot profile exit reservoir
+        res_values = np.asarray(cropped_exit_reservoir.getdata())
+        values_res = res_values[:, 0]
+
+        # attributing grayscale value to a phase
+        exit_res_dodecane = len(list((filter(lambda dod: dod <= 40, values_res))))  # black
+        exit_res_krytox = len(list((filter(lambda kryt: 41 <= kryt <= 247, values_res))))  # gray
+
         # appending data to dataframe
         if not cumul_krytox == 0:
             new_row = pd.DataFrame(
-                {"time [s]": [i * 1e-4], "krytox": [cumul_krytox], "dodecane": [cumul_dodecane],
-                 "cumul_none": [cumul_none], "normalized_krytox": [cumul_krytox / (cumul_krytox + cumul_dodecane)]},
+                {"time [s]": [i * 1e-4], "cumul Krytox": [cumul_krytox], "cumul dodecane": [cumul_dodecane],
+                 "cumul_none": [cumul_none], "normalized_krytox": [cumul_krytox / (cumul_krytox + cumul_dodecane)],
+                 "pore dodecane": [pore_dodecane], "pore Krytox": [pore_krytox],
+                 "exit reservoir Krytox": [exit_res_krytox], "exit reservoir dodecane": [exit_res_dodecane],
+                 "inlet reservoir Krytox": [cumul_krytox - pore_krytox - exit_res_krytox],
+                 "inlet reservoir dodecane": [cumul_dodecane - pore_dodecane - pore_krytox]},
                 index=[i])
             df_master = pd.concat([df_master, new_row])
 
         else:
             new_row = pd.DataFrame(
-                {"time [s]": [i * 1e-4], "krytox": [cumul_krytox], "dodecane": [cumul_dodecane],
-                 "cumul_none": [cumul_none], "normalized_krytox": [0]}, index=[i])
+                {"time [s]": [i * 1e-4], "cumul Krytox": [cumul_krytox], "cumul dodecane": [cumul_dodecane],
+                 "cumul_none": [cumul_none], "normalized_krytox": [0], "pore dodecane": [pore_dodecane],
+                 "pore Krytox": [pore_krytox], "exit reservoir Krytox": [exit_res_krytox],
+                 "exit reservoir dodecane": [exit_res_dodecane],
+                 "inlet reservoir Krytox": [cumul_krytox - pore_krytox - exit_res_krytox],
+                 "inlet reservoir dodecane": [cumul_dodecane - pore_dodecane - pore_krytox]}, index=[i])
             df_master = pd.concat([df_master, new_row])
 
         # plot this funky stuff
@@ -72,6 +105,6 @@ for filename in file_list:
 
 # save to a file
 dir_name = os.path.join(os.getcwd())
-df_master.to_csv(path_or_buf=directory + folder_name + ".csv", sep=';')
+df_master.to_csv(path_or_buf=directory + folder_name + "_pore_res.csv", sep=';')
 plt.show()
 plt.savefig(folder_name + "_plot.png")
